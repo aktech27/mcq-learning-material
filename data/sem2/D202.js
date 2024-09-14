d07ada06ddc069fee6e013738ea04057086313ac
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2024-09-14 11:05:57 +0530

    Folder restructure for SEM2

---
+++ data/sem2/D202.js
@@ -0,0 +1,58 @@
+const unit1 = [];
+
+const unit2 = [];
+
+const unit3 = [];
+
+const unit4 = [];
+
+const unit5 = [];
+
+const unit6 = [];
+
+const unit7 = [];
+
+const unit8 = [];
+
+const unit9 = [];
+
+const unit10 = [];
+
+const unit11 = [];
+
+const unit12 = [];
+
+const unit13 = [];
+
+const unit14 = [];
+
+const unit15 = [];
+
+const ca1 = [];
+
+const ca2 = [];
+
+const ca3 = [];
+
+const compiled = [
+  ...unit1,
+  ...unit2,
+  ...unit3,
+  ...unit4,
+  ...unit5,
+  ...unit6,
+  ...unit7,
+  ...unit8,
+  ...unit9,
+  ...unit10,
+  ...unit11,
+  ...unit12,
+  ...unit13,
+  ...unit14,
+  ...unit15,
+  ...ca1,
+  ...ca2,
+  ...ca3,
+];
+
+
